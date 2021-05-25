04e411af2751e7973a8c96100faca11812d7db3c
cnzau <clement.nzau@gmail.com>
2021-05-25 14:05:05 +0300

    Search patient and get more data

--- dbService.js
+++ dbService.js
@@ -23,6 +23,128 @@ class DbService {
   static getDbServiceInstance() {
     return instance ? instance : new DbService();
   }
+
+  async getAllPatients(...args) {
+    const { search_name } = args[0];
+    const condition = (ct) =>
+      ct != null ? `WHERE name LIKE '%${search_name}%'` : '';
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = `SELECT 
+              patient.patient_id AS ID,
+              patient.name AS Name,
+              SUBSTRING(patient.gender, 1, 1) AS Gender,
+              DATE_FORMAT(patient.dob, '%Y-%m-%d') AS DOB,
+              DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), patient.dob)),
+                      '%Y') + 0 AS Age,
+              patient.phone_number AS Contact,
+              DATE_FORMAT(patient.date_created, '%Y-%m-%d') AS 'Date Created'
+          FROM
+              patient ${condition(search_name)};`;
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // async searchByName(name) {
+  //   try {
+  //     const response = await new Promise((resolve, reject) => {
+  //       const query = `SELECT
+  //             patient.patient_id AS ID,
+  //             name as Name,
+  //             gender as Gender,
+  //             DATE_FORMAT(dob,'%Y-%m-%d') AS DOB,
+  //             DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), patient.dob)),
+  //                     '%Y') + 0 AS Age,
+  //             phone_number AS Contact
+  //         FROM
+  //             patient
+  //         WHERE name LIKE '%${name}%';`;
+  //       connection.query(query, [name], (err, results) => {
+  //         if (err) reject(new Error(err.message));
+  //         resolve(results);
+  //       });
+  //     });
+
+  //     return response;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // get patient details
+  async getPatient(id) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = `SELECT 
+              patient.patient_id AS ID,
+              patient.name as Name,
+              patient.gender as Gender,
+              DATE_FORMAT(dob,'%Y-%m-%d') AS DOB,
+              DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), patient.dob)),
+                      '%Y') + 0 AS Age,
+              phone_number AS Contact
+          FROM
+              patient
+          WHERE
+              patient.patient_id = ${id};`;
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // get patient data/reports
+  async getPatientReport(id) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = `SELECT 
+              DATE_FORMAT(flat_cdm_summary.encounter_datetime,
+                      '%Y-%m-%d') AS 'Encounter Date',
+              location.name AS Location,
+              (CASE
+                  WHEN flat_cdm_summary.htn_status = 7285 THEN 'New'
+                  WHEN flat_cdm_summary.htn_status = 7286 THEN 'Known'
+                  ELSE NULL
+              END) AS 'Hypertension Status',
+              (CASE
+                  WHEN flat_cdm_summary.dm_status = 7281 THEN 'New'
+                  WHEN flat_cdm_summary.dm_status = 7282 THEN 'Known'
+                  ELSE NULL
+              END) AS 'Diabetic Status'
+          FROM
+              patient
+                  INNER JOIN
+              flat_cdm_summary ON patient.patient_id = flat_cdm_summary.patient_id
+                  INNER JOIN
+              location ON location.id = flat_cdm_summary.location_id
+          WHERE
+              patient.patient_id = ${id};`;
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 module.exports = DbService;
--- server.js
+++ server.js
@@ -16,6 +16,52 @@ app.get('/', (req, res) =>
   res.json({ msg: 'Chronic Disease Management system' })
 );
 
+// Get patients or search by ? search_name
+app.get('/patients', (request, response) => {
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.getAllPatients({ ...request.query });
+
+  result
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.log(err));
+});
+
+// app.get('/patients/search/:name', (request, response) => {
+//   const { name } = request.params;
+//   const db = dbService.getDbServiceInstance();
+
+//   const result = db.searchByName(name);
+
+//   result
+//     .then((data) => response.json({ data: data }))
+//     .catch((err) => console.log(err));
+// });
+
+// get a patient's data
+app.get('/patients/:id', (request, response) => {
+  const { id } = request.params;
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.getPatient(id);
+
+  result
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.log(err));
+});
+
+// get a patient's reports
+app.get('/patients/:id/reports', (request, response) => {
+  const { id } = request.params;
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.getPatientReport(id);
+
+  result
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.log(err));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
